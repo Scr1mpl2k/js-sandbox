1ec0c01892776fa9e237d0827083a16119f0637e
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-01 23:30:49 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello');
+const fistName = 'Vanya';
+const lastName = 'shsh'
+let age = 33
+console.log(fistName + lastName);
+age = 44+32;
+console.log(age);  
+alert(fistName + ' '+ lastName); 
