153c220d26a96873154301a9ec958cb8c0ea1989
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-06 00:42:13 +0300

    latest

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ switch (vanya) {
 //let luckyNumber = numbers[numbers.leght - 1];
 //console.log(numbers[numbers.length - 1]);  говно на обсуждение
 
+const numbers = [2, 7, 19, 22, 38, 9];
+let index = 0;
+while (index < numbers.length) {
+    numbers[index] % 2 !== 0 ? index++ : console.log(numbers[index]*=2);
+    
+    
+    //if (index % 2 !== 0) {
+     //   numbers[index] = numbers[index] * 2
+        
+//}
+  //  index++;
+}
+
+console.log(numbers);
