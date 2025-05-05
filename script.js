a88e7872000c2807e810a2fb40b8138cabc304e2
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-05 18:43:12 +0300

    йобанныеМассивы

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-(x) => isOdd = x % 2 === 0 ? false : true;
+let vanya = 'hyinya';
+switch (vanya) {
+    case 'name' :
+        console.log('eto imya');
+        break;
+    case 'hyinya' : 
+        console.log('NAME KURVA');
+        break;
+    default:
+        console.log('IVAAAAAAN')
+        break;
+}
+ let show = [5, 6, 7, 8];
+ console.log(show.length);
+ console.log(show[show.length -1]);
 
-checkIsOdd(2);
-console.log(isOdd);
+ const strs = ['abc', 'bcd' , 5];
 
-checkIsOdd(3);
-console.log(isOdd);
+ console.log(strs.length);
+
+ strs.push(15, 5, 4);
+ console.log(strs);
+ console.log(strs.pop());
+ console.log(strs);
+ 
+
+
+//const numbers = [1, 2, 3];
+//let luckyNumber = numbers[numbers.leght - 1];
+//console.log(numbers[numbers.length - 1]);  говно на обсуждение
+
