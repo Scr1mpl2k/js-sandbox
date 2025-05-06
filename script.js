cc9b097f92da24a02606c50589a01db9ab4f13a7
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-06 15:18:52 +0300

    for

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-let vanya = 'hyinya';
-switch (vanya) {
-    case 'name' :
-        console.log('eto imya');
-        break;
-    case 'hyinya' : 
-        console.log('NAME KURVA');
-        break;
-    default:
-        console.log('IVAAAAAAN')
-        break;
+const numbers = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i < numbers.length; i++) {
+    i % 2 === 0 ? numbers[i] = numbers[i] + 3 : null;
 }
- let show = [5, 6, 7, 8];
- console.log(show.length);
- console.log(show[show.length -1]);
-
- const strs = ['abc', 'bcd' , 5];
-
- console.log(strs.length);
-
- strs.push(15, 5, 4);
- console.log(strs);
- console.log(strs.pop());
- console.log(strs);
- 
-
-
-//const numbers = [1, 2, 3];
-//let luckyNumber = numbers[numbers.leght - 1];
-//console.log(numbers[numbers.length - 1]);  говно на обсуждение
-
-const numbers = [2, 7, 19, 22, 38, 9];
-let index = 0;
-while (index < numbers.length) {
-    numbers[index] % 2 !== 0 ? index++ : console.log(numbers[index]*=2);
-    
-    
-    //if (index % 2 !== 0) {
-     //   numbers[index] = numbers[index] * 2
-        
-//}
-  //  index++;
-}
-
 console.log(numbers);
