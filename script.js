0dd9178130071857d056b2b7a6514393c9577b09
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-17 14:37:06 +0300

    not work

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-//
-const fruits = ['apple', 'banana', 'melon', 'orange'];
-const hasOrange = search(fruits, 'orange');
-function search(a, b) { 
-    const  searchA = a.includes(b);
-    return(searchA);
+function addByX(a) {
+    const rar = () =>{
+        console.log(a + addByX());
+    }
+    return rar;
 }
-
-const cars = ['BMW', 'Nissan', 'VW', 'Skoda', 'Audi', 'Kia'];
-const favoriteCars = cars.slice(0 , 3);
-const otherCars = cars.slice(3);
-console.log(favoriteCars);
-console.log(otherCars);
-console.log('nice');
+const addByTwo = addByX(2);
+const addByFour  = addByX(4)
+addByTwo(5);
