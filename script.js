7dcf33e322adb5bb8106b5404c525ca04084bfd0
Scr1mpl2k <scr1mpl2k@gmail.com>
2025-05-26 19:11:55 +0300

    dont undertand

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const hi = 'vanyaaaaaaaaaaaaaaaaaa'
+const hi = 'hello world'
 const calculateLetters = (str) => {
     let push = {};
     const newA = Array.from(str)
     for (let i = 0; i < newA.length; i++) {
         const item = newA[i];
+        if(item === ' '){
+            continue;
+        }
         if(push[item]){
             push[item] += 1;
         }else{
